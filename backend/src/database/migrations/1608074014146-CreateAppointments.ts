53d4c8f0b7951c241803f5286aa956fc336e66bf
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-12-16 20:08:56 -0300

    relationshipModel

--- backend/src/database/migrations/1608074014146-CreateAppointments.ts
+++ backend/src/database/migrations/1608074014146-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1608074014146
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- backend/src/database/migrations/1608101269458-CreateUsers.ts
+++ backend/src/database/migrations/1608101269458-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1608101269458 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
