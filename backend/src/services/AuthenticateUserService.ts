c1f6294c74af449ea859d398f8e672f5022b0fc4
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-12-17 03:25:59 -0300

    TokenJWT

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -34,8 +36,15 @@ class AuthenticateUserService {
       throw new Error('Incorreto email/password.');
     }
 
+    // User Autenticado => Gerar um token
+    const token = sign({}, '8a85c2b72edc73beac185409f01aff70', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
