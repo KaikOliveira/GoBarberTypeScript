e6d21f90aea9fc11f19dfb4ca9df497fec768026
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-12-29 04:40:11 -0300

    apiContexto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,15 @@ import SignIn from './pages/SigIn/index';
 // import SignUp from './pages/SigUp/index';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
-    <GlobalStyle />,
+    <AuthContext.Provider value={{ name: 'Kaik' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
+    <GlobalStyle />
   </>
 );
 
