7ce9b5e3faad70f1e012c84f567de9e5ad5802e6
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-12-29 22:56:23 -0300

    contextLogin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ import './database';
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
--- frontend/src/content/AuthContext.tsx
+++ frontend/src/content/AuthContext.tsx
@@ -1,15 +1,33 @@
-import React, { createContext } from 'react';
+import React, { createContext, useCallback } from 'react';
+import api from '../services/api';
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
 
 interface AuthContextData {
   name: string;
+  singIn(credentials: SignInCredentials): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-export const AuthProvider: React.FC = ({ children }) => {
+const AuthProvider: React.FC = ({ children }) => {
+  const singIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    console.log(response.data);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ name: 'Kaik' }}>
+    <AuthContext.Provider value={{ name: 'Kaik', singIn }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+export { AuthContext, AuthProvider };
