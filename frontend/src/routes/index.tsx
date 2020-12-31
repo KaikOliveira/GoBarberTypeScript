e52aff9ee7f2218703c684dcc425df167ca1b3b9
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2020-12-31 02:24:55 -0300

    routesAuthtenticate

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SigIn';
 import SignUp from '../pages/SigUp';
+
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
