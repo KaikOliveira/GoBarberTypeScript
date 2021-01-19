ca203f1f54d3f2bae66821bdb603c8e8c6fd6c9a
KaikOliveira <69028381+KaikOliveira@users.noreply.github.com>
2021-01-19 04:55:42 -0300

    screen auth OK

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <Container>
+          <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password"icon="lock"placeholder="Senha" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password"icon="lock"placeholder="Senha" />
 
-      <Button onPress={() =>{}} >Entrar</Button>
-    </Container>
+          <Button onPress={() =>{}} >Entrar</Button>
+
+          <ForgotPassword onPress={() => {}}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </KeyboardAvoidingView>
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
 export default SignIn;
+
