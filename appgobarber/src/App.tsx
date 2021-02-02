03f0b12cbc3ef9f65a9b705ce2c1b31bbbf904bd
KaikOliveira <69028381+KaikOliveira@users.noreply.github.com>
2021-02-02 20:43:45 -0300

    eslintAndPrettier

---
+++ appgobarber/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const App: React.FC = () => (
+  <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+);
+export default App;
