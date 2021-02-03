8e51c37993734377c420ef4494ae6ee7ca54ca86
KaikOliveira <69028381+KaikOliveira@users.noreply.github.com>
2021-02-02 22:11:48 -0300

    logoAndTypes

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -9,8 +9,9 @@ import Routes from './routes';
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-    <Routes />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
   </NavigationContainer>
 );
 export default App;
